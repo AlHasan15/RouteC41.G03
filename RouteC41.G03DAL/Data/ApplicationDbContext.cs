aeb21f5fea607a7a03630a6addc53bc5b75257b1
AlHasan15 <alhassanrasmy1@gmail.com>
2024-03-21 09:16:02 +0200

    Data Access Layer

---
+++ RouteC41.G03DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouteC41.G03DAL.Data.Configrations;
+using RouteC41.G03DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC41.G03DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server =.; Database = MVCApplicationG03; Trusted_connection =True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations() );
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
